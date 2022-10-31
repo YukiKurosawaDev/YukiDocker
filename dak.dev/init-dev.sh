4715e6938ba774e62d90ca1f1a1cd0f5e00b2ee8
Yuki Kurosawa HotKey <yuki@ksyuki.com>
2022-10-31 16:15:35 +0800

    Set DAK ENV

--- dak.dev/init-dev.sh
+++ dak.dev/init-dev.sh
@@ -2,7 +2,7 @@
 
 echo -n "CONFIGURING DAK ENVIRONMENT ... "
 echo 'export PATH="/srv/dak/bin:${PATH}"' > ~/.bashrc
-
+echo 'export PATH="/srv/dak/bin:${PATH}"' > /home/dak/.bashrc
 source ~/.bashrc
 
 USER_CMD="sudo -E -u dak -s -H"
@@ -26,9 +26,9 @@ echo "DONE"
 echo -n "INITING AN EMPTY REPO ... "
 $USER_CMD $DAK admin architecture add amd64 "KSLinux 22.04.1 AMD64" 1>/dev/null 2>&1
 
-$USER_CMD $DAK admin suite add-all-arches focal 22.04.1 origin=KSLinux label=Focal codename=focal signingkey=451DD5811062DFC93DF54EEC259531ED17EE37C1 1>/dev/null 2>&1
+$USER_CMD $DAK admin suite add-all-arches jammy 22.04.1 origin=KSLinux label=Focal codename=focal signingkey=451DD5811062DFC93DF54EEC259531ED17EE37C1 1>/dev/null 2>&1
 
-$USER_CMD $DAK admin s-c add focal main restricted universe multiverse 1>/dev/null 2>&1
+$USER_CMD $DAK admin s-c add jammy main restricted universe multiverse 1>/dev/null 2>&1
 
 $USER_CMD $DAK init-dirs 1>/dev/null 2>&1
 
