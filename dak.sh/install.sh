0876a4dba7b5bf917ec64fe0baf2e66d95cf4730
Yuki Kurosawa HotKey <yuki@ksyuki.com>
2022-10-31 17:44:11 +0800

    Add Debootstrap for test

--- dak.sh/install.sh
+++ dak.sh/install.sh
@@ -39,6 +39,10 @@ echo -n "INSTALLING nano ... "
 apt install nano -y 1>/dev/null 2>&1
 echo DONE
 
+echo -n "INSTALLING debootstrap ... "
+apt install debootstrap -y 1>/dev/null 2>&1
+echo DONE
+
 echo -n "INSTALLING dak Dependencies ... "
 apt install python3-psycopg2 python3-pip python3-apt gnupg dpkg-dev lintian \
 binutils-multiarch python3-yaml less python3-ldap python3-pyrss2gen python3-rrdtool \
