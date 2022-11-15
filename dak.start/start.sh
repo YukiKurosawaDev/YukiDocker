7fe3e8d737edea5ce2a19ba8d73a608bd1649fb3
Yuki Kurosawa HotKey <yuki@ksyuki.com>
2022-11-15 16:34:59 +0800

    No Need to set /etc/* config files

--- dak.start/start.sh
+++ dak.start/start.sh
@@ -135,44 +135,44 @@ $DAK generate-release 1>/dev/null 2>&1
 debootstrap --no-check-gpg jammy /test http://localhost/kslinux
 cp -r /test.tmp /test/pkgs
 
-cat > /test1/etc/passwd << "EOF"
-root:x:0:0:root:/root:/bin/bash
-bin:x:1:1:bin:/dev/null:/usr/bin/false
-daemon:x:6:6:Daemon User:/dev/null:/usr/bin/false
-messagebus:x:18:18:D-Bus Message Daemon User:/run/dbus:/usr/bin/false
-uuidd:x:80:80:UUID Generation Daemon User:/dev/null:/usr/bin/false
-nobody:x:65534:65534:Unprivileged User:/dev/null:/usr/bin/false
-EOF
+# cat > /test1/etc/passwd << "EOF"
+# root:x:0:0:root:/root:/bin/bash
+# bin:x:1:1:bin:/dev/null:/usr/bin/false
+# daemon:x:6:6:Daemon User:/dev/null:/usr/bin/false
+# messagebus:x:18:18:D-Bus Message Daemon User:/run/dbus:/usr/bin/false
+# uuidd:x:80:80:UUID Generation Daemon User:/dev/null:/usr/bin/false
+# nobody:x:65534:65534:Unprivileged User:/dev/null:/usr/bin/false
+# EOF
 
-cat > /test1/etc/group << "EOF"
-root:x:0:
-bin:x:1:daemon
-sys:x:2:
-kmem:x:3:
-tape:x:4:
-tty:x:5:
-daemon:x:6:
-floppy:x:7:
-disk:x:8:
-lp:x:9:
-dialout:x:10:
-audio:x:11:
-video:x:12:
-utmp:x:13:
-usb:x:14:
-cdrom:x:15:
-adm:x:16:
-messagebus:x:18:
-input:x:24:
-mail:x:34:
-kvm:x:61:
-uuidd:x:80:
-wheel:x:97:
-users:x:999:
-nogroup:x:65534:
-EOF
+# cat > /test1/etc/group << "EOF"
+# root:x:0:
+# bin:x:1:daemon
+# sys:x:2:
+# kmem:x:3:
+# tape:x:4:
+# tty:x:5:
+# daemon:x:6:
+# floppy:x:7:
+# disk:x:8:
+# lp:x:9:
+# dialout:x:10:
+# audio:x:11:
+# video:x:12:
+# utmp:x:13:
+# usb:x:14:
+# cdrom:x:15:
+# adm:x:16:
+# messagebus:x:18:
+# input:x:24:
+# mail:x:34:
+# kvm:x:61:
+# uuidd:x:80:
+# wheel:x:97:
+# users:x:999:
+# nogroup:x:65534:
+# EOF
 
-touch /test/etc/shadow
+# touch /test/etc/shadow
 
 chroot /test /bin/bash
 
