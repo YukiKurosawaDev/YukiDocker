076a91c3b97c3af6393c18e820cb326f0a92e1ec
Yuki Kurosawa HotKey <yuki@ksyuki.com>
2023-08-28 16:51:06 +0800

    Remove Legacy Images

--- update.bat
+++ update.bat
@@ -2,5 +2,4 @@
 docker pull ubuntu:jammy
 docker pull mcr.microsoft.com/dotnet/sdk:7.0-jammy
 docker pull mcr.microsoft.com/dotnet/sdk:6.0-jammy
-docker pull mcr.microsoft.com/dotnet/sdk:3.1-focal
 docker pull rabbitmq:latest
