c56fc774f08db710d6d2295ed471e54eb0b3d0d2
Yuki Kurosawa HotKey <yuki@ksyuki.com>
2023-09-18 16:23:16 +0800

    Add gzip for DAK

--- update.bat
+++ update.bat
@@ -1,5 +1,6 @@
 @echo off
 docker pull ubuntu:jammy
+docker pull ubuntu:mantic
 docker pull mcr.microsoft.com/dotnet/sdk:7.0-jammy
 docker pull mcr.microsoft.com/dotnet/sdk:6.0-jammy
 docker pull rabbitmq:latest
