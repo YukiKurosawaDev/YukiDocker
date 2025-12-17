9c3454ada3a4e0a11974c7d9d1e9c069e9ccb300
Yuki Kurosawa Hotkey <yuki@ksyuki.com>
2025-12-17 15:34:07 +0800

    remove clean and compact disk scripts

--- cleandisk.bat
+++
@@ -1,4 +0,0 @@
-select vdisk file="C:\Users\Yuki\AppData\Local\Docker\wsl\data\ext4.vhdx"
-attach vdisk readonly
-compact vdisk
-detach vdisk
--- cleanwsl.bat
+++
@@ -1,4 +0,0 @@
-select vdisk file="C:\Users\Yuki\AppData\Local\Packages\CanonicalGroupLimited.Ubuntu22.04LTS_79rhkp1fndgsc\LocalState\ext4.vhdx"
-attach vdisk readonly
-compact vdisk
-detach vdisk
